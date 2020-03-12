5e14f5203d9522367594ca497d06f59670625854
Team-on <timonsol13@gmail.com>
2020-03-12 02:27:50 +0200

    Settings menu template

--- GameTemplate/Assets/Scripts/UI/Menu/MainMenu.cs
+++ GameTemplate/Assets/Scripts/UI/Menu/MainMenu.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenu : MenuBase {
+	public void Play() {
+		SceneLoader.Instance.LoadScene("SampleScene2D", true, true);
 
+	}
+
+	public void Load() {
+		SceneLoader.Instance.LoadScene("SampleScene2D", true, true);
+	}
 }
---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/UI/Collections/TabControlButton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class TabControlButton : MonoBehaviour {
+	[SerializeField] Color selectedColor;
+	Color defaultColor;
+
+	Button button;
+	bool isDisabledByDefault;
+
+	Action<byte> onClick;
+	byte id;
+
+	public void Init(Action<byte> _onClick, byte _id) {
+		button = GetComponent<Button>();
+		defaultColor = button.colors.disabledColor;
+		isDisabledByDefault = !button.interactable;
+
+		if (isDisabledByDefault)
+			return;
+
+		onClick = _onClick;
+		id = _id;
+		button.onClick.AddListener(OnClick);
+	}
+
+	void OnClick() {
+		if (isDisabledByDefault)
+			return;
+
+		onClick?.Invoke(id);
+	}
+
+	public void SetSelection(bool isSelected) {
+		if (isDisabledByDefault)
+			return;
+
+		var c = button.colors;
+		if (isSelected) 
+			c.disabledColor = selectedColor;
+		else
+			c.disabledColor = defaultColor;
+		button.colors = c;
+
+		button.interactable = !isSelected;
+	}
+}
