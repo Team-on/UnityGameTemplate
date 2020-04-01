09b3dd86f67065e13d04cf4281f838f05b930249
Team-on <timonsol13@gmail.com>
2020-04-01 03:01:34 +0300

    Prevent MoveToDontDestroyOnload to create dublicates

--- GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Singleton/MoveToDontDestroyOnload.cs
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Singleton/MoveToDontDestroyOnload.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveToDontDestroyOnload : MonoBehaviour {
+	static List<string> placed = new List<string>();
+
 	void Awake() {
+		if (placed.Contains(name)) {
+			Destroy(gameObject);
+			return;
+		}
+
+		placed.Add(name);
 		DontDestroyOnLoad(gameObject);
 		Destroy(this);
 	}
