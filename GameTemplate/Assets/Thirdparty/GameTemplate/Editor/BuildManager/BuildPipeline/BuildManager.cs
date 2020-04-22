c72650d563bff999f41badc933f3b38a9ebdb763
Team <timonsol13@gmail.com>
2020-04-23 02:17:04 +0300

    Add few more names to build manager

--- GameTemplate/Assets/Thirdparty/GameTemplate/Editor/BuildManager/BuildPipeline/BuildManager.cs
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Editor/BuildManager/BuildPipeline/BuildManager.cs
@@ -12,10 +12,12 @@ using Debug = UnityEngine.Debug;
 
 public static class BuildManager {
 	const string butlerRelativePath = @"Thirdparty/GameTemplate/Editor/BuildManager/butler/butler.exe";
+	static DateTime usedDate;
 
 	public static void RunBuildSequnce(BuildSequence sequence, ChangelogData changelog) {
 		// Start init
 		GameManager.InstanceEditor.buildNameString = $"{PlayerSettings.bundleVersion} - {changelog.LocalizedUpdate}";
+		usedDate = DateTime.Now;
 		//End init
 
 		Debug.Log("Start building all");
@@ -69,7 +71,13 @@ public static class BuildManager {
 		s = s.Replace("$NAME", GetProductName());
 		s = s.Replace("$PLATFORM", ConvertBuildTargetToString(data.target));
 		s = s.Replace("$VERSION", PlayerSettings.bundleVersion);
-		s = s.Replace("$DATE", $"{DateTime.Now.Date.Year}_{DateTime.Now.Date.Month}_{DateTime.Now.Date.Day}");
+		s = s.Replace("$DATESHORT", $"{usedDate.Date.Year % 100}_{usedDate.Date.Month}_{usedDate.Date.Day}");
+		s = s.Replace("$YEARSHORT", $"{usedDate.Date.Year % 100}");
+		s = s.Replace("$DATE", $"{usedDate.Date.Year}_{usedDate.Date.Month}_{usedDate.Date.Day}");
+		s = s.Replace("$YEAR", $"{usedDate.Date.Year}");
+		s = s.Replace("$MONTH", $"{usedDate.Date.Month}");
+		s = s.Replace("$DAY", $"{usedDate.Date.Day}");
+		s = s.Replace("$TIME", $"{usedDate.Hour}_{usedDate.Minute}");
 		s = s.Replace("$EXECUTABLE", GetBuildTargetExecutable(data.target));
 		return s;
 	}
@@ -185,7 +193,7 @@ public static class BuildManager {
 
 		args.Append($"{sequence.itchGameLink}:{data.itchChannel} ");
 		if (data.itchAddLastChangelogUpdateNameToVerison && !string.IsNullOrEmpty(data.itchLastChangelogUpdateName)) {
-			args.Append($"--userversion \"{PlayerSettings.bundleVersion} - {data.itchLastChangelogUpdateName}\" ");
+			args.Append($"--userversion \"{data.itchLastChangelogUpdateName}\" ");
 		}
 		else {
 			args.Append($"--userversion \"{PlayerSettings.bundleVersion}\" ");
