c03eec865e9e79b7482d933f752218a3db44cd62
Team <timonsol13@gmail.com>
2020-05-05 03:01:57 +0300

    Add simple sprites animators

---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Animators/ImageAnimator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+
+[RequireComponent(typeof(Image))]
+public class ImageAnimator : MonoBehaviour {
+	[SerializeField] bool startWithRandom = true;
+	[SerializeField] bool stopAfterAnim = false;
+	[SerializeField] float secondsForOneSprite = 0.35f;
+	[SerializeField] [ReorderableList] Sprite[] sprites = null;
+	[ReadOnly] [SerializeField] Image img = null;
+
+	byte currSprite = 0;
+	float time = 0;
+
+#if UNITY_EDITOR
+	private void OnValidate() {
+		if (img == null)
+			img = GetComponent<Image>();
+	}
+#endif
+
+	private void Awake() {
+		if (startWithRandom) {
+			currSprite = (byte)Random.Range(0, sprites.Length);
+			img.sprite = sprites[currSprite];
+			time = Random.Range(0, secondsForOneSprite - Time.deltaTime);
+		}
+		else {
+			img.sprite = sprites[currSprite];
+		}
+	}
+
+	void Update() {
+		time += Time.deltaTime;
+		if(time >= secondsForOneSprite) {
+			time -= secondsForOneSprite;
+			++currSprite;
+			if (currSprite == sprites.Length) {
+				if (stopAfterAnim) {
+					enabled = false;
+					return;
+				}
+				currSprite = 0;
+			}
+			img.sprite = sprites[currSprite];
+		}
+	}
+
+	public float GetDuration() {
+		return secondsForOneSprite * sprites.Length + secondsForOneSprite / 2;
+	}
+}
---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Animators/SpriteRendererAnimator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteRendererAnimator : MonoBehaviour {
+	[SerializeField] bool startWithRandom = true;
+	[SerializeField] float secondsForOneSprite = 0.35f;
+	[SerializeField] [ReorderableList] Sprite[] sprites = null;
+	[ReadOnly] [SerializeField] SpriteRenderer sr = null;
+
+	byte currSprite = 0;
+	float time = 0;
+
+#if UNITY_EDITOR
+	private void OnValidate() {
+		if (sr == null)
+			sr = GetComponent<SpriteRenderer>();
+	}
+#endif
+
+	private void Start() {
+		if (startWithRandom) {
+			currSprite = (byte)Random.Range(0, sprites.Length);
+			sr.sprite = sprites[currSprite];
+			time = Random.Range(0, secondsForOneSprite - Time.deltaTime);
+		}
+		else {
+			sr.sprite = sprites[currSprite];
+		}
+	}
+
+	void Update() {
+		time += Time.deltaTime;
+		if(time >= secondsForOneSprite) {
+			time -= secondsForOneSprite;
+			++currSprite;
+			if (currSprite == sprites.Length)
+				currSprite = 0;
+			if (sr)
+				sr.sprite = sprites[currSprite];
+		}
+	}
+}
