519847fe7bb1022041a356ccd12cd630e87a6a6f
Team <timonsol13@gmail.com>
2020-05-22 16:05:50 +0300

    More ultis scripts

---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Ultis/KeepZeroRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepZeroRotation : MonoBehaviour
+{
+    static Quaternion zeroQuaterion = Quaternion.Euler(Vector3.zero);
+
+    private void LateUpdate() {
+        transform.rotation = zeroQuaterion;
+    }
+}
---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Ultis/Rotating.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour {
+	[SerializeField] float rotationSpeed = 180f;
+
+	private void Awake() {
+		transform.Rotate(0, 0, Random.Range(0f, 360f));
+	}
+
+	void Update() {
+		transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+	}
+}
