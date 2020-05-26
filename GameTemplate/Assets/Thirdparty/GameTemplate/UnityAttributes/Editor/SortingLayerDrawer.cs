fb185d4abc867b6e5cd53a702508c520ebdb792a
Team <timonsol13@gmail.com>
2020-05-26 20:31:18 +0300

    Add attributes for Layer and SortingLayer

---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/UnityAttributes/Editor/SortingLayerDrawer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Linq;
+
+[CustomPropertyDrawer(typeof(SortingLayerAttribute))]
+public class SortingLayerDrawer : PropertyDrawer {
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+		var sortingLayerNames = SortingLayer.layers.Select(l => l.name).ToArray();
+		if (property.propertyType != SerializedPropertyType.Integer) {
+			EditorGUI.HelpBox(position, string.Format("{0} is not an integer but has [SortingLayer].", property.name), MessageType.Error);
+		}
+		else if (sortingLayerNames != null) {
+			EditorGUI.BeginProperty(position, label, property);
+
+			// Look up the layer name using the current layer ID
+			string oldName = SortingLayer.IDToName(property.intValue);
+
+			// Use the name to look up our array index into the names list
+			int oldLayerIndex = Array.IndexOf(sortingLayerNames, oldName);
+
+			// Show the popup for the names
+			int newLayerIndex = EditorGUI.Popup(position, label.text, oldLayerIndex, sortingLayerNames);
+
+			// If the index changes, look up the ID for the new index to store as the new ID
+			if (newLayerIndex != oldLayerIndex) {
+				property.intValue = SortingLayer.NameToID(sortingLayerNames[newLayerIndex]);
+			}
+
+			EditorGUI.EndProperty();
+		}
+		else {
+			EditorGUI.BeginProperty(position, label, property);
+			int newValue = EditorGUI.IntField(position, label.text, property.intValue);
+			if (newValue != property.intValue) {
+				property.intValue = newValue;
+			}
+			EditorGUI.EndProperty();
+		}
+	}
+}
---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/UnityAttributes/LayerAttribute.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Attribute to select a single layer.
+/// </summary>
+public class LayerAttribute : PropertyAttribute {
+    // NOTHING - just oxygen.
+}
