5f59533b484a79f8b7c489f583abba066a1c0c3e
Team <timonsol13@gmail.com>
2020-07-27 22:23:15 +0300

    More utils classes

---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Ultis/LookAtMainCamera.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMainCamera : MonoBehaviour {
+	void Update() {
+		transform.rotation = (GameManager.Instance.Camera.transform.rotation);
+	}
+}
---
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Ultis/PlayOnClick.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayOnClick : MonoBehaviour {
+	[SerializeField] AudioClip clip; 
+	[SerializeField] Button button;
+
+#if UNITY_EDITOR
+	private void OnValidate() {
+		if (button == null)
+			button = GetComponent<Button>();
+	}
+#endif
+
+	private void Awake() {
+		if(button != null) {
+			button.onClick.AddListener(OnClick);
+		}
+	}
+
+	private void OnDestroy() {
+		if (button != null) {
+			button.onClick.RemoveListener(OnClick);
+		}
+	}
+
+	void OnClick() {
+		AudioManager.Instance.Play(clip, channel: AudioManager.AudioChannel.Sound);
+	}
+}
