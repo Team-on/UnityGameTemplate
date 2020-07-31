621cca86536326c3cd085cca94eafb24592e28be
Team <timonsol13@gmail.com>
2020-08-01 02:47:52 +0300

    Fix Ultis to work without domain reload

--- GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Ultis/MoveToDontDestroyOnload.cs
+++ GameTemplate/Assets/Thirdparty/GameTemplate/Scripts/Ultis/MoveToDontDestroyOnload.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MoveToDontDestroyOnload : MonoBehaviour {
 	static List<string> placed = new List<string>();
+
+#if UNITY_EDITOR	
+	static MoveToDontDestroyOnload() {
+	EditorApplication.playModeStateChanged += (PlayModeStateChange state) => {
+			if(state == PlayModeStateChange.ExitingPlayMode) {
+				placed.Clear();
+			}
+		};
+	}
+#endif
 
 	void Awake() {
 		if (placed.Contains(name)) {
