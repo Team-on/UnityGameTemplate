33210cbda05a9ff14a13065b72ed9553254ad961
Team <timonsol13@gmail.com>
2020-08-23 05:51:19 +0300

    Better AudioManager

--- GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/LeanTweenEx.cs
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/LeanTweenEx.cs
@@ -97,3 +97,4 @@ public static class LeanTweenEx {
 		}
 		action?.Invoke();
 	}
+}
--- GameTemplate/Assets/Plugins/GameTemplate/Scripts/Singleton/AudioManager.cs
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Singleton/AudioManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
@@ -55,8 +56,10 @@ public class AudioManager : Singleton<AudioManager> {
 	public float defaultSoundVolume = 1;
 	public bool defaultEnabled = true;
 
-	AudioClip musicClip;
-	AudioSource musicSource;
+	Dictionary<AudioClip, AudioSource> musicAudioSources;
+	AudioClip currMusicClip;
+	AudioClip lastMusicClip;
+	float lastMusicVolume;
 
 	protected override void Initialize() {
 		base.Initialize();
@@ -74,6 +77,9 @@ public class AudioManager : Singleton<AudioManager> {
 		}
 	}
 
+
+	//--------------------------------------------------------------------------------------
+	// General
 	/// <param name="volume">[0..1]</param>
 	public void SetVolume(AudioChannel channel, float volume) {
 		float adjustedVolume = GetAdjustedVolume(volume);
@@ -108,42 +114,87 @@ public class AudioManager : Singleton<AudioManager> {
 		return 0.0f;
 	}
 
+	//--------------------------------------------------------------------------------------
+	//Music
 	public void PlayMusic(AudioClip clip, float volume = 1.0f) {
-		if(clip != musicClip || musicSource == null) {
-			AudioSource oldSource = musicSource;
-			musicClip = clip;
-			musicSource = PlayLoop(musicClip, volume, playDelay: crossfadeTime);
-			DontDestroyOnLoad(musicSource.gameObject);
+		if (currMusicClip != clip) {
+			AudioSource oldSource = currMusicClip != null ? musicAudioSources[currMusicClip] : null;
+			currMusicClip = clip;
 
-			if (oldSource != null) {
-				LeanTween.cancel(oldSource.gameObject, false);
-				LeanTween.value(oldSource.gameObject, oldSource.volume, 0.0f, crossfadeTime)
-				.setOnUpdate((float v) => {
-					oldSource.volume = v;
-				})
-				.setOnComplete(() => {
-					Destroy(oldSource.gameObject);
-				});
+			lastMusicClip = currMusicClip;
+			lastMusicVolume = volume;
+
+			if (currMusicClip != null) {
+				if (musicAudioSources.ContainsKey(currMusicClip) && musicAudioSources[currMusicClip] != null) {
+					AudioSource newAS = musicAudioSources[currMusicClip];
+					ChangeASVolume(newAS, volume);
+				}
+				else {
+					musicAudioSources[currMusicClip] = PlayLoop(currMusicClip, volume, playDelay: crossfadeTime);
+					DontDestroyOnLoad(musicAudioSources[currMusicClip].gameObject);
+				}
 			}
+
+			ChangeASVolume(oldSource, 0.0f);
 		}
 	}
 
-	public AudioSource Play3D(AudioClip clip, Transform emitter, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Sound) {
-		if (!IsEnabled && channel != AudioChannel.Music)
-			return null;
-		AudioSource source = CreatePlaySource3D(clip, emitter, volume, pitch, playDelay, channel);
-		Destroy(source.gameObject, clip.length + 1.0f);
+	public void MuteMusic(float time = 0.5f) {
+		if (currMusicClip == null)
+			return;
+
+		AudioSource source = musicAudioSources[currMusicClip];
+		if (source == null)
+			return;
+
+		ChangeASVolume(source, 0.0f, time);
+
+		currMusicClip = null;
+	}
+
+	public void ContinueMusicAfterMute() {
+		PlayMusic(lastMusicClip, lastMusicVolume);
+	}
+
+	public void DeleteAllPlayedMusic(float delay = 0.0f) {
+		if (delay == 0) {
+			DeleteMusicInner();
+		}
+		else {
+			LeanTween.delayedCall(delay, DeleteMusicInner);
+		}
+
+		void DeleteMusicInner() {
+			foreach (var musicAudioSource in musicAudioSources)
+				Destroy(musicAudioSource.Value.gameObject);
+			musicAudioSources.Clear();
+
+			lastMusicClip = currMusicClip = null;
+			lastMusicVolume = 0.0f;
+		}
+	}
+
+	public AudioSource PlayLoop(AudioClip clip, Transform emitter, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Music) {
+		AudioSource source = CreatePlaySource(clip, emitter, volume, pitch, playDelay, channel);
+		source.loop = true;
 		return source;
 	}
 
-	public AudioSource Play3D(AudioClip clip, Vector3 point, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Sound) {
-		if (!IsEnabled && channel != AudioChannel.Music)
-			return null;
-		AudioSource source = CreatePlaySource3D(clip, point, volume, pitch, playDelay, channel);
-		Destroy(source.gameObject, clip.length + 1.0f);
+	public AudioSource PlayLoop(AudioClip clip, Vector3 point, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Music) {
+		AudioSource source = CreatePlaySource(clip, point, volume, pitch, playDelay, channel);
+		source.loop = true;
 		return source;
 	}
 
+	public AudioSource PlayLoop(AudioClip clip, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Music) {
+		AudioSource source = CreatePlaySource(clip, Vector3.zero, volume, pitch, playDelay, channel);
+		source.loop = true;
+		return source;
+	}
+
+
+	//--------------------------------------------------------------------------------------
+	//2D sound
 	public AudioSource Play(AudioClip clip, Transform emitter, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Sound) {
 		if (!IsEnabled && channel != AudioChannel.Music)
 			return null;
@@ -165,24 +216,6 @@ public class AudioManager : Singleton<AudioManager> {
 			return null;
 		AudioSource source = CreatePlaySource(clip, Vector3.zero, volume, pitch, playDelay, channel);
 		Destroy(source.gameObject, clip.length + 1.0f);
-		return source;
-	}
-
-	public AudioSource PlayLoop(AudioClip clip, Transform emitter, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Music) {
-		AudioSource source = CreatePlaySource(clip, emitter, volume, pitch, playDelay, channel);
-		source.loop = true;
-		return source;
-	}
-
-	public AudioSource PlayLoop(AudioClip clip, Vector3 point, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Music) {
-		AudioSource source = CreatePlaySource(clip, point, volume, pitch, playDelay, channel);
-		source.loop = true;
-		return source;
-	}
-
-	public AudioSource PlayLoop(AudioClip clip, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Music) {
-		AudioSource source = CreatePlaySource(clip, Vector3.zero, volume, pitch, playDelay, channel);
-		source.loop = true;
 		return source;
 	}
 
@@ -217,10 +250,29 @@ public class AudioManager : Singleton<AudioManager> {
 		return source;
 	}
 
+
+	//--------------------------------------------------------------------------------------
+	//3D sound
+	public AudioSource Play3D(AudioClip clip, Transform emitter, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Sound) {
+		if (!IsEnabled && channel != AudioChannel.Music)
+			return null;
+		AudioSource source = CreatePlaySource3D(clip, emitter, volume, pitch, playDelay, channel);
+		Destroy(source.gameObject, clip.length + 1.0f);
+		return source;
+	}
+
+	public AudioSource Play3D(AudioClip clip, Vector3 point, float volume = 1.0f, float pitch = 1.0f, float playDelay = 0.0f, AudioChannel channel = AudioChannel.Sound) {
+		if (!IsEnabled && channel != AudioChannel.Music)
+			return null;
+		AudioSource source = CreatePlaySource3D(clip, point, volume, pitch, playDelay, channel);
+		Destroy(source.gameObject, clip.length + 1.0f);
+		return source;
+	}
+
 	AudioSource CreatePlaySource3D(AudioClip clip, Transform emitter, float volume, float pitch, float playDelay, AudioChannel channel) {
 		GameObject go = new GameObject("Audio: " + clip.name);
 		go.transform.parent = emitter;
-		if(is3DGame)
+		if (is3DGame)
 			go.transform.position = emitter.position;
 		else
 			go.transform.position = new Vector3(emitter.position.x, emitter.position.y, TemplateGameManager.Instance.Camera.transform.position.z);
@@ -233,7 +285,7 @@ public class AudioManager : Singleton<AudioManager> {
 
 	AudioSource CreatePlaySource3D(AudioClip clip, Vector3 point, float volume, float pitch, float playDelay, AudioChannel channel) {
 		GameObject go = new GameObject("Audio: " + clip.name);
-		if(is3DGame)
+		if (is3DGame)
 			go.transform.position = point;
 		else
 			go.transform.position = new Vector3(point.x, point.y, TemplateGameManager.Instance.Camera.transform.position.z);
@@ -249,7 +301,7 @@ public class AudioManager : Singleton<AudioManager> {
 		source.clip = clip;
 		source.pitch = pitch;
 		source.outputAudioMixerGroup = GetAudioMixer(channel);
-		
+
 		source.minDistance = 0.0f;
 		source.maxDistance = maxSoundDistance;
 		source.spatialBlend = 1.0f;
@@ -261,22 +313,22 @@ public class AudioManager : Singleton<AudioManager> {
 		return source;
 	}
 
+
+	//--------------------------------------------------------------------------------------
+	//Inner helpers 
 	float GetAdjustedVolume(float volume) {
 		return volume <= 0.0001f ? -80f : Mathf.Log10(volume) * 20;
 	}
 
 	void PlayDelayed(AudioSource source, float delay) {
-		if (delay == 0)
+		if (delay == 0) {
 			source.Play();
+		}
 		else {
 			float savedVolume = source.volume;
 			source.volume = 0.0f;
 			source.Play();
-			LeanTween.cancel(source.gameObject, false);
-			LeanTween.value(source.gameObject, source.volume, savedVolume, delay)
-				.setOnUpdate((float v)=> { 
-					source.volume = v;
-				});
+			ChangeASVolume(source, savedVolume, delay);
 		}
 	}
 
@@ -290,6 +342,25 @@ public class AudioManager : Singleton<AudioManager> {
 				return soundGroup;
 			default:
 				return null;
+		}
+	}
+
+	void ChangeASVolume(AudioSource source, float volume) {
+		ChangeASVolume(source, volume, crossfadeTime);
+	}
+
+	void ChangeASVolume(AudioSource source, float volume, float time) {
+		if (source != null) {
+			LeanTween.cancel(source.gameObject, false);
+			if (time == 0) {
+				source.volume = volume;
+			}
+			else {
+				LeanTween.value(source.gameObject, source.volume, volume, time)
+				.setOnUpdate((float v) => {
+					source.volume = v;
+				});
+			}
 		}
 	}
 }
