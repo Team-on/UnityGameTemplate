5c8588d0acf8bd84c81fe83ba053ae8621fa6919
Team <timonsol13@gmail.com>
2020-08-25 11:11:28 +0300

    More utils

---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/Physics2DExtensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+ public static class Physics2DExtensions {
+     public static void AddForce (this Rigidbody2D rigidbody2D, Vector2 force, ForceMode2D mode = ForceMode2D.Force) {
+         switch (mode) {
+         case ForceMode2D.Force:
+             rigidbody2D.AddForce (force);
+             break;
+         case ForceMode2D.Impulse:
+             rigidbody2D.AddForce (force / Time.fixedDeltaTime);
+             break;
+         case ForceMode2D.Acceleration:
+             rigidbody2D.AddForce (force * rigidbody2D.mass);
+             break;
+         case ForceMode2D.VelocityChange:
+             rigidbody2D.AddForce (force * rigidbody2D.mass / Time.fixedDeltaTime);
+             break;
+         }
+     }
+     
+     public static void AddForce (this Rigidbody2D rigidbody2D, float x, float y, ForceMode2D mode = ForceMode2D.Force) {
+         rigidbody2D.AddForce (new Vector2 (x, y), mode);
+     }
+ }
+
+public enum ForceMode2D : byte {
+    Force,
+    Impulse,
+    Acceleration,
+    VelocityChange,
+}
---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Ultis/ScaleUpDown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScaleUpDown : MonoBehaviour {
+	[Header("Params")]
+	[SerializeField] float minScale = 0.75f;
+	[SerializeField] float maxScale = 1.0f;
+	[SerializeField] float maxTime = 0.5f;
+
+	float currTime;
+	bool isIncreaseScale = false;
+
+	void Update() {
+		if (isIncreaseScale) {
+			currTime += Time.deltaTime;
+			if (currTime >= maxTime) {
+				currTime = maxTime;
+				isIncreaseScale = false;
+			}
+		}
+		else {
+			currTime -= Time.deltaTime;
+			if (currTime <= 0) {
+				currTime = 0;
+				isIncreaseScale = true;
+			}
+		}
+
+		gameObject.transform.localScale = Vector3.one * Mathf.SmoothStep(0.66f, 1.0f, currTime / maxTime);
+	}
+}
