eb7958f9b5d9b8a2641c78e78faf4d1e49e8d22b
Team <timonsol13@gmail.com>
2020-09-02 01:47:37 +0300

    Add TransformEx - destroy all childrens

---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/TransformEx.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public static class TransformEx {
+    public static Transform DestroyAllChildrens(this Transform transform) {
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        transform.DetachChildren();
+
+        return transform;
+    }
+}
