57e6874a492ee78618c93ad0c28f903906cbb0eb
Team <timonsol13@gmail.com>
2020-09-25 06:34:49 +0300

    Add collider2d extensions

---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/Collider2DEx.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public static class Collider2DEx {
+	public static Vector2 GetRandomWorldPointInsideCollider(this Collider2D collider) {
+		var bounds = collider.bounds;
+		var center = bounds.center;
+		float minX = center.x - bounds.extents.x;
+		float maxX = center.x + bounds.extents.x;
+		float minY = center.y - bounds.extents.y;
+		float maxY = center.y + bounds.extents.y;
+
+		Vector2 rez;
+		int tries = 100;
+		do {
+			rez = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+		} while (!collider.OverlapPoint(rez) && --tries >= 0);
+
+		return rez;
+	}
+
+	public static Vector2 GetRandomLocalPointInsideCollider(this Collider2D collider) {
+		return GetRandomWorldPointInsideCollider(collider) - (Vector2)collider.transform.position;
+	}
+}
