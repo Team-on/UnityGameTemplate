3ccde1c8265a0f978e363ff87288f0dda26744ba
Team <timonsol13@gmail.com>
2020-09-25 07:45:38 +0300

    Local force for Physics2D

--- GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/Physics2DEx.cs
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/Physics2DEx.cs
@@ -17,8 +17,25 @@
              break;
          }
      }
-     
-     public static void AddForce (this Rigidbody2D rigidbody2D, float x, float y, ForceMode2DEx mode = ForceMode2DEx.Force) {
+
+    public static void AddRelativeForce(this Rigidbody2D rigidbody2D, Vector2 force, ForceMode2DEx mode = ForceMode2DEx.Force) {
+        switch (mode) {
+            case ForceMode2DEx.Force:
+                rigidbody2D.AddRelativeForce(force);
+                break;
+            case ForceMode2DEx.Impulse:
+                rigidbody2D.AddRelativeForce(force / Time.fixedDeltaTime);
+                break;
+            case ForceMode2DEx.Acceleration:
+                rigidbody2D.AddRelativeForce(force * rigidbody2D.mass);
+                break;
+            case ForceMode2DEx.VelocityChange:
+                rigidbody2D.AddRelativeForce(force * rigidbody2D.mass / Time.fixedDeltaTime);
+                break;
+        }
+    }
+
+    public static void AddForce (this Rigidbody2D rigidbody2D, float x, float y, ForceMode2DEx mode = ForceMode2DEx.Force) {
          rigidbody2D.AddForce (new Vector2 (x, y), mode);
      }
  }
