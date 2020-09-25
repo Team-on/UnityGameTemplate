9bcc87b1449b1173ea2fda0858092a1b5566a9fe
Team <timonsol13@gmail.com>
2020-09-25 07:06:27 +0300

    Add animator extensions

---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/AnimatorEx.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AnimatorEx {
+	public static bool ContainsParam(this Animator _Anim, string _ParamName) {
+		foreach (AnimatorControllerParameter param in _Anim.parameters) {
+			if (param.name == _ParamName)
+				return true;
+		}
+		return false;
+	}
+}
