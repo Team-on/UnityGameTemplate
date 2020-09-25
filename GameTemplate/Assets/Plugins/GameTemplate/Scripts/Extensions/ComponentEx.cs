38138f77005a4b1e95738d3a7fb83c508b8f9a28
Team <timonsol13@gmail.com>
2020-09-25 06:55:48 +0300

    Add component extensions

---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/ComponentEx.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public static class ComponentEx {
+	public static T GetComponentThisOrParent<T>(this GameObject go) where T : Component {
+		T val = default(T);
+		if (val == null) {
+			val = go.GetComponent<T>();
+			if (val == null)
+				val = go.GetComponentInParent<T>();
+		}
+		return val;
+	}
+
+	public static T GetComponentThisOrParent<T>(this Component comp) where T : Component {
+		T val = default(T);
+		if (val == null) {
+			val = comp.GetComponent<T>();
+			if (val == null)
+				val = comp.GetComponentInParent<T>();
+		}
+		return val;
+	}
+}
