be2ec93cfea2c611931a80f1d6314b58b90b842a
Team <timonsol13@gmail.com>
2020-09-25 07:43:14 +0300

    Add selectable extensions

---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/SelectableEx.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public static class SelectableEx {
+	public static void SetNavigationUp(this Selectable selectable, Selectable target) {
+		Navigation nav = selectable.navigation;
+		nav.selectOnUp = target;
+		selectable.navigation = nav;
+	}
+
+	public static void SetNavigationDown(this Selectable selectable, Selectable target) {
+		Navigation nav = selectable.navigation;
+		nav.selectOnDown = target;
+		selectable.navigation = nav;
+	}
+
+	public static void SetNavigationLeft(this Selectable selectable, Selectable target) {
+		Navigation nav = selectable.navigation;
+		nav.selectOnLeft = target;
+		selectable.navigation = nav;
+	}
+
+	public static void SetNavigationRight(this Selectable selectable, Selectable target) {
+		Navigation nav = selectable.navigation;
+		nav.selectOnRight = target;
+		selectable.navigation = nav;
+	}
+}
