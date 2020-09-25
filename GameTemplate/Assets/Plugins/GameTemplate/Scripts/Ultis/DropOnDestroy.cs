f7a2c146aaba1fd497df1600fc5725d4614ba7b3
Team <timonsol13@gmail.com>
2020-09-25 07:44:09 +0300

    More utils scripts

---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Ultis/DropOnDestroy.cs
@@ -0,0 +1,53 @@
+﻿using NaughtyAttributes;
+using UnityEngine;
+
+namespace Utils {
+	public class DropOnDestroy : MonoBehaviour {
+		[Header("Drop")]
+		[Space]
+		[SerializeField] Transform[] positions = null;
+		[SerializeField] GameObject[] drops = null;
+		[SerializeField] byte[] chance = null;
+
+		[Header("Drop forces")]
+		[Space]
+		[SerializeField] [MinMaxSlider(0, 10, false)] Vector2 dropForce = new Vector2(1, 3);
+
+		[Header("This refs")]
+		[Space]
+		[SerializeField] Rigidbody2D rb = null;
+
+#if UNITY_EDITOR
+		private void OnValidate() {
+			if (rb == null)
+				rb = GetComponent<Rigidbody2D>();
+		}
+#endif
+
+		// Called in Health.cs by BroadcastMessage("Die")
+		void Die() {
+			int usedPos = 0;
+			positions.Shuffle();
+
+			for (byte i = 0; i < drops.Length && i < chance.Length; ++i) {
+				if (RandomEx.GetEventWithChance(chance[i])) {
+					if (drops[i] == null) {
+						Debug.LogError($"Can't spawn {drops[i]}, prefab is null");
+						continue;
+					}
+
+					GameObject go = Instantiate(drops[i], positions[usedPos].position, Quaternion.Euler(0, 0, Random.Range(0, 360)), transform.parent);
+
+					Rigidbody2D objectRb = go.GetComponent<Rigidbody2D>();
+					if (objectRb) {
+						objectRb.velocity += rb.velocity;
+						objectRb.velocity += (Vector2)positions[usedPos].localPosition.normalized * dropForce.GetRandomValue();
+						objectRb.angularVelocity += rb.angularVelocity;
+					}
+
+					++usedPos;
+				}
+			}
+		}
+	}
+}
---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Ultis/SpawnRandomPrefab.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SpawnRandomPrefab : MonoBehaviour {
+	[SerializeField] GameObject[] prafabs;
+
+	void Start() {
+		if (prafabs != null && prafabs.Length != 0) {
+			Instantiate(prafabs.Random(), transform.position, Quaternion.identity, transform);
+		}
+		Destroy(this);
+	}
+}
