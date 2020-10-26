8da429d8e1d31629f320131081d85014002e596b
Team-on <timonsol13@gmail.com>
2020-10-26 19:11:17 +0200

    Remove unusud methods

--- GameTemplate/Assets/Plugins/ScreenShooter/Editor/ScreenshotTakerEditor.cs
+++ GameTemplate/Assets/Plugins/ScreenShooter/Editor/ScreenshotTakerEditor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -233,64 +232,5 @@ public static class ScreenshotTakerEditor {
 
 	private static Type GetType(string type) {
 		return typeof(EditorWindow).Assembly.GetType("UnityEditor." + type);
-	}
-
-	private static object FetchField(this object obj, string field) {
-		return obj.GetType().GetFieldRecursive(field, false).GetValue(obj);
-	}
-
-	private static object FetchProperty(this Type type, string property) {
-		return type.GetPropertyRecursive(property, true).GetValue(null, null);
-	}
-
-	private static object FetchProperty(this object obj, string property) {
-		return obj.GetType().GetPropertyRecursive(property, false).GetValue(obj, null);
-	}
-
-	private static object CallMethod(this object obj, string method, params object[] parameters) {
-		return obj.GetType().GetMethod(method, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Invoke(obj, parameters);
-	}
-
-	private static object CreateInstance(this Type type, params object[] parameters) {
-		Type[] parameterTypes;
-		if (parameters == null)
-			parameterTypes = null;
-		else {
-			parameterTypes = new Type[parameters.Length];
-			for (int i = 0; i < parameters.Length; i++)
-				parameterTypes[i] = parameters[i].GetType();
-		}
-
-		return CreateInstance(type, parameterTypes, parameters);
-	}
-
-	private static object CreateInstance(this Type type, Type[] parameterTypes, object[] parameters) {
-		return type.GetConstructor(parameterTypes).Invoke(parameters);
-	}
-
-	private static FieldInfo GetFieldRecursive(this Type type, string field, bool isStatic) {
-		BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly | (isStatic ? BindingFlags.Static : BindingFlags.Instance);
-		do {
-			FieldInfo fieldInfo = type.GetField(field, flags);
-			if (fieldInfo != null)
-				return fieldInfo;
-
-			type = type.BaseType;
-		} while (type != null);
-
-		return null;
-	}
-
-	private static PropertyInfo GetPropertyRecursive(this Type type, string property, bool isStatic) {
-		BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly | (isStatic ? BindingFlags.Static : BindingFlags.Instance);
-		do {
-			PropertyInfo propertyInfo = type.GetProperty(property, flags);
-			if (propertyInfo != null)
-				return propertyInfo;
-
-			type = type.BaseType;
-		} while (type != null);
-
-		return null;
 	}
 }
