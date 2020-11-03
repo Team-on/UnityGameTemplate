5c4c26c4d26bcf4f90df5698148356eb3185725d
Team-on <timonsol13@gmail.com>
2020-11-03 12:55:49 +0200

    Fix scele loading

--- GameTemplate/Assets/Plugins/GameTemplate/Scripts/UI/DefaultMenu/Menu/TemplateMainMenu.cs
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/UI/DefaultMenu/Menu/TemplateMainMenu.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TemplateMainMenu : MenuBase {
+	[SerializeField] string sceneToLoad = "SampleScene2D";
+
 	public void Play() {
-		SceneLoader.Instance.LoadScene("SampleScene2D", true, true);
+		SceneLoader.Instance.LoadScene(sceneToLoad, true, true);
 
 	}
 
 	public void Load() {
-		SceneLoader.Instance.LoadScene("SampleScene2D", true, false);
+		SceneLoader.Instance.LoadScene(sceneToLoad, true, true);
 	}
 }
