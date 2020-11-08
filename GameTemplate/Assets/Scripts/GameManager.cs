9b4dccbda92a4994cc8a7416c1024c593c1fa64b
Team-on <timonsol13@gmail.com>
2020-11-08 19:15:08 +0200

    Default game manager

---
+++ GameTemplate/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using NaughtyAttributes;
+using yaSingleton;
+using Random = UnityEngine.Random;
+
+[CreateAssetMenu(fileName = "GameManager", menuName = "Singletons/GameManager")]
+public class GameManager : Singleton<GameManager> {
+
+
+	protected override void Initialize() {
+		base.Initialize();
+	}
+
+	protected override void Deinitialize() {
+		base.Deinitialize();
+	}
+}
