c2c2f1df4afeae6a608c2f43d94c4eb96d0a4b4d
Team <timonsol13@gmail.com>
2020-12-28 20:31:30 +0200

    Add bar UI from https://github.com/savik-games/GenesisGameJam

---
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/UI/DefaultElements/BarUI.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BarUI : MonoBehaviour {
+	[Header("Refs"), Space]
+	[SerializeField] Canvas canvas = null;
+	[SerializeField] Slider barFirst = null;
+	[SerializeField] Slider barSecond = null;
+	[SerializeField] TextMeshProUGUI textField = null;
+
+	[Header("Timing"), Space]
+	[SerializeField] float firstBarTime = 0.2f;
+	[SerializeField] float secondBarTime = 1.0f;
+
+	float currValue, maxValue;
+
+	public void Init(int _currValue, int _maxValue) {
+		currValue = _currValue;
+		maxValue = _maxValue;
+
+		ReinitBars();
+	}
+
+	public void SetValue(int newValue) {
+		currValue = newValue;
+		UpdateBar();
+	}
+
+	public void ChangeValue(int change) {
+		currValue += change;
+		UpdateBar();
+	}
+
+	public void SetMaxValue(int newValue) {
+		maxValue = newValue;
+		ReinitBars();
+		UpdateBar();
+	}
+
+	public void ChangeMaxValue(int change) {
+		maxValue += change;
+		ReinitBars();
+		UpdateBar();
+	}
+
+	void ReinitBars() {
+		barFirst.minValue = 0.0f;
+		barFirst.maxValue = maxValue;
+		barFirst.value = currValue;
+
+		barSecond.minValue = 0.0f;
+		barSecond.maxValue = maxValue;
+		barSecond.value = currValue;
+
+		if (textField != null)
+			textField.text = $"{Mathf.RoundToInt(currValue)}/{Mathf.RoundToInt(maxValue)}";
+	}
+
+
+	void UpdateBar() {
+		LeanTween.cancel(barFirst.gameObject, false);
+
+		LeanTween.value(barFirst.gameObject, barFirst.value, currValue, firstBarTime)
+		.setEase(LeanTweenType.linear)
+		.setOnUpdate((float val) => {
+			barFirst.value = val;
+		});
+
+		LeanTween.cancel(barSecond.gameObject, false);
+		LeanTween.value(barSecond.gameObject, barSecond.value, currValue, secondBarTime)
+		.setEase(LeanTweenType.easeInQuart)
+		.setOnUpdate((float val) => {
+			barSecond.value = val;
+		});
+
+		if (textField != null)
+			textField.text = $"{Mathf.RoundToInt(currValue)}/{Mathf.RoundToInt(maxValue)}";
+	}
+}
