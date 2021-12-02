4e178b5b47a83729e9bbf8daf412243860e1eaba
Team-on <timonsol13@gmail.com>
2021-12-02 11:28:46 +0200

    Random ex

--- GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/RandomEx.cs
+++ GameTemplate/Assets/Plugins/GameTemplate/Scripts/Extensions/RandomEx.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public static class RandomEx {
 	public static bool GetEventWithChance(int percent) {
@@ -37,5 +37,41 @@ public static class RandomEx {
 		if (array.Length == 0)
 			return default(T);
 		return array[UnityEngine.Random.Range(0, array.Length)];
+	}
+
+	public static Vector2 PointInCircle() {
+		float angle = UnityEngine.Random.Range(0, 360);
+		float radius = UnityEngine.Random.Range(Mathf.Epsilon, 1f);
+		Vector2 randomPos = new Vector3(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle));
+
+		return randomPos;
+	}
+
+	public static Vector3 PointInSphere() {
+		float u = UnityEngine.Random.Range(0f, 1f);
+		float v = UnityEngine.Random.Range(0f, 1f);
+		float radius = UnityEngine.Random.Range(Mathf.Epsilon, 1f);
+
+		float theta = 2 * Mathf.PI * u;
+		float phi = Mathf.Acos(2 * v - 1);
+
+		float sinPhi = Mathf.Sin(phi);
+		float cosPhi = Mathf.Cos(phi);
+		float sinTheta = Mathf.Sin(theta);
+		float cosTheta = Mathf.Cos(theta);
+
+		float x = (radius * sinPhi * cosTheta);
+		float y = (radius * sinPhi * sinTheta);
+		float z = (radius * cosPhi);
+
+		return new Vector3(x, y, z);
+	}
+
+	public static Vector2 PointInCircleNormalized() {
+		return PointInCircle().normalized;
+	}
+
+	public static Vector3 PointInSphereNormalized() {
+		return PointInSphere().normalized;
 	}
 }
